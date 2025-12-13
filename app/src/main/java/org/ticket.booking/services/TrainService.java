37ce454173b84d3197d0acec1f1b240add9621eb
SNEHA SRI ADUPA <117506229+snehasri16@users.noreply.github.com>
2025-12-13 08:12:05 +0000

    commit ch2

---
+++ app/src/main/java/org/ticket.booking/services/TrainService.java
@@ -0,0 +1,5 @@
+package org.ticket.booking.services;
+
+public class TrainService {
+
+}
--- app/src/main/java/org/ticket.booking/services/UserBookingService.java
+++ app/src/main/java/org/ticket.booking/services/UserBookingService.java
@@ -1,5 +1,14 @@
 package org.ticket.booking.services;
 
+import java.io.File;
+
 public class UserBookingService {
 
+    private User user;
+    private static final String USERS_PATH="../localStorage/users.json";
+    public UserBookingService(User user1){
+        this.user=user1;
+        File userFile= new File(USERS_PATH);
+        
+    }
 }
