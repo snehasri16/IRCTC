1b7fcf5bfedebf742be05d0e955301ea6ba9b70e
SNEHA SRI ADUPA <117506229+snehasri16@users.noreply.github.com>
2025-12-16 05:59:10 +0000

    entities updated

--- app/src/main/java/org/ticket.booking/services/UserBookingService.java
+++ app/src/main/java/org/ticket.booking/services/UserBookingService.java
@@ -1,14 +1,36 @@
 package org.ticket.booking.services;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.io.IOException;
 
 public class UserBookingService {
 
     private User user;
+    private ObjectMapper ObjectMapper= new ObjectMapper();
+    
     private static final String USERS_PATH="../localStorage/users.json";
-    public UserBookingService(User user1){
+
+    public UserBookingService(User user1) throws IOException{
         this.user=user1;
         File userFile= new File(USERS_PATH);
+        userList = ObjectMapper.readValue(users, new TypeReference<List<User>>(){});
         
+
+    }
+     public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
     }
 }
